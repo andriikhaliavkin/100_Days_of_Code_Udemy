cd7df0d4d3faad11bb09d7e28c919e0ec7a7d5a7
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-09-20 11:54:11 +0200

    Day 1 assessment: Band name's generator

---
+++ day1.py
@@ -0,0 +1,5 @@
+#Band name's generator
+print("Wellcome to the Bend Name Generator")
+city_name = input("What's name of the city You grew up in?: ")
+pet_name = input("What's your pet's name?: ")
+print(f"Your band name could be {city_name + ' ' + pet_name}")
