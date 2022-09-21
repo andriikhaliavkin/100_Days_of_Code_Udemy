b9f5bf1a165ef82ff0ac3f5b1217751fa89a493c
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-09-21 23:52:06 +0200

    Day 3 init

--- day2.py
+++ day2.py
@@ -3,3 +3,4 @@ bill = float(input("What was the total bill?: "))
 tip = int(input("What percentage tip would You like to give? 10, 12 or 15?: "))
 people = int(input("How many people to split the bill?: "))
 print(f"Each person should pay {round((bill * (1 + tip/100))/people)}$")
+
